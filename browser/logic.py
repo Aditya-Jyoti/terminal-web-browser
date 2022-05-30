dcdd6f5fb1cbe64b8301ce62380f4ff41ab93c8e
reverend-toady <rev.toady.py@gmail.com>
2022-05-31 01:28:36 +0530

    Add basic keyboard interaction and UI stuff

--- browser/logic.py
+++ browser/logic.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from typing import List
 from urllib.parse import urlparse
 from html2text import html2text
+from os import name, system
 
 
 def search_page_content(search_query: str) -> List[List[str]]: 
@@ -34,6 +35,13 @@ def search_page_content(search_query: str) -> List[List[str]]:
     return page_content
 
 def read_page_content(link_to_page: str):
+    """
+    Reads the urls provided and prints out a pretty version of the text contained in the website 
+
+    Args:
+        link_to_page: [str] -> https://domain_name/other_text
+    """
+
     html_content = requests.get(link_to_page).text 
     
     soup = BeautifulSoup(html_content, "html.parser")
@@ -42,5 +50,23 @@ def read_page_content(link_to_page: str):
 
     print(html2text(soup.prettify()))
 
-     
-    
+def clear_screen():
+    """
+    cleans the terminal space, works for both windows and non windows machines 
+    """
+
+    if name == "nt":
+        _ = system("cls")
+        print("\n"*100)
+        print("THE TERMINAL WAS CLEARED HERE")
+        print("\n"*10)
+        _ = system("cls")
+
+    else:
+        _ = system("clear")
+        print("\n"*100)
+        print("THE TERMINAL WAS CLEARED HERE")
+        print("\n"*10)
+        _ = system("clear")
+
+
--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-from browser.logic import search_page_content, read_page_content
+from browser.logic import search_page_content, read_page_content, clear_screen
 from browser.user_interactions import ask_link_to_open, ask_search_query
+from time import sleep
+import keyboard
 
-read_page_content(ask_link_to_open(search_page_content(ask_search_query()))[1])
+def main():
+    search_link = ask_search_query()
+    sleep(0.2)
+    clear_screen()
+    website_link = ask_link_to_open(search_page_content(search_link))
+    print(f"\nopening link: {website_link[1]}")
+    sleep(0.2)
+    read_page_content(website_link[1])
+    while True:
+        if keyboard.is_pressed("ctrl+s"):
+            clear_screen()
+            main()
+     
+
+
+if __name__ == "__main__":
+    print(r"""
+     _    _ ___________  ____________ _____  _    _ _____ ___________ 
+    | |  | |  ___| ___ \ | ___ \ ___ \  _  || |  | /  ___|  ___| ___ \
+    | |  | | |__ | |_/ / | |_/ / |_/ / | | || |  | \ `--.| |__ | |_/ /
+    | |/\| |  __|| ___ \ | ___ \    /| | | || |/\| |`--. \  __||    / 
+    \  /\  / |___| |_/ / | |_/ / |\ \\ \_/ /\  /\  /\__/ / |___| |\ \ 
+     \/  \/\____/\____/  \____/\_| \_|\___/  \/  \/\____/\____/\_| \_|
+    """)
+    
+    print("Press [ctrl+s] key to go back to the search page")
+    sleep(1)
+    clear_screen()
+    main()
