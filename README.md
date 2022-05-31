640f3a25b382b5bb8a8a27ef25bebd75b6ca0e8f
reverend-toady <rev.toady.py@gmail.com>
2022-05-31 13:59:36 +0530

    Finish beta release of project

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # terminal-web-browser
+
+This project was made to emulate a web browser in a terminal. Its extreamly
+barebones as of yet and still has many more features planned for it. As of yet
+it works partially for reading webpage content and displaying it in a sort of
+markdown fashion.
+
+This is project is still in its **Beta Phase**.
+
+#### Known Issues:
+
+As of writing this since I'm using the `html2text` library to convert my html
+responses to text, there are times when text tends to be ignored. Currently on
+visiting a wikipedia page no real text is displayed out and on `a` tag links are
+visible, though I don't know why.
+
+#### How To Run It:
+
+```
+1) Clone the repository
+Note: since the keyboard module is being used for linux machines root access is required
+
+2) run `&sudo pip3 install -r requirements.txt`
+3) run `&sudo python3 main.py`
+
+Note: the above commands may change for windows and there may not be a need to use `sudo`
+      use at your own discreation
+```
+
+_Since I'm on `ubuntu` I'm not exactly sure if Windows users can run the program
+or not._
+
+#### Keyboard Shortcuts:
+
+1. `ctrl+s`: returns back to the search query page
+2. `ctrl+b`: returns back to the page containting search query links
+3. `ctrl+q`: quits the program
+
+**Note: all keyboard shortcuts work only in non entry field times, ie, when user
+input is not being asked**
+
+#### After Thoughts:
+
+I would appreciate your feedback on this project and would like if you could
+open issues suggesting how to fix the known issues, as well as contricbuting to
+the project to make this even better
+
--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 from browser.logic import search_page_content, read_page_content, clear_screen
 from browser.user_interactions import ask_link_to_open, ask_search_query
 from time import sleep
+from sys import exit
 import keyboard
+
+def displaying_search_page(search_link: str):
+    """
+    displays webpage content from a provided search term
+
+    Args:
+        search_link: [str] -> https://domain_name/other_text
+    """
+
+    website_link = ask_link_to_open(search_page_content(search_link))
+    print(f"\nopening link: {website_link[1]}")
+    sleep(0.2)
+    clear_screen()
+    read_page_content(website_link[1])
 
 def main():
     search_link = ask_search_query()
     sleep(0.2)
     clear_screen()
-    website_link = ask_link_to_open(search_page_content(search_link))
-    print(f"\nopening link: {website_link[1]}")
-    sleep(0.2)
-    read_page_content(website_link[1])
+    displaying_search_page(search_link)
+
     while True:
         if keyboard.is_pressed("ctrl+s"):
             clear_screen()
             main()
-     
+        
+        if keyboard.is_pressed("ctrl+b"):
+            clear_screen()
+            displaying_search_page(search_link)
+            
+        if keyboard.is_pressed("ctrl+q"):
+            clear_screen()
+            print("QUITTING PROGRAM")
+            return
 
 
 if __name__ == "__main__":
+    clear_screen()
     print(r"""
      _    _ ___________  ____________ _____  _    _ _____ ___________ 
     | |  | |  ___| ___ \ | ___ \ ___ \  _  || |  | /  ___|  ___| ___ \
@@ -27,8 +49,25 @@ if __name__ == "__main__":
     \  /\  / |___| |_/ / | |_/ / |\ \\ \_/ /\  /\  /\__/ / |___| |\ \ 
      \/  \/\____/\____/  \____/\_| \_|\___/  \/  \/\____/\____/\_| \_|
     """)
-    
-    print("Press [ctrl+s] key to go back to the search page")
-    sleep(1)
-    clear_screen()
+
+    print() 
+    print("This is a extreamly barebones terminal emulation of a web browser")
+    print("Works best when trying to read pages which contain maximum text")
+    print("Keyboard Shotcuts:- ")
+    print("     1) ctrl+s -> Open up prompt to ask for search query")
+    print("     2) ctrl+b -> Return back to main page containing links to search results")
+    print("     3) ctrl+q -> Quit the program")
+    print()
+    print("Please press enter when you want to continue")
+    print("Please press ctrl+q to quit the program")
+
+    while True:
+        if keyboard.is_pressed("enter"):
+            filler = input()
+            clear_screen()
+            break 
+        
+        if keyboard.is_pressed("ctrl+q"):
+            exit(0)
+
     main()
